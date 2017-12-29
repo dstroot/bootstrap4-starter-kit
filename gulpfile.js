93c4ebd62dd8444479ce3305578da732753b14e8
Dan Stroot <dan.stroot@gmail.com>
2017-12-29 12:05:56 -0800

    gulp es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-'use strict';
 /* exported dotenv */
 
 // Install: you must install gulp both globally *and* locally.
@@ -8,38 +7,41 @@
  * Dependencies
  */
 
-var $           = require('gulp-load-plugins')({ lazy: true });
-var os          = require('os');
-var del         = require('del');
-var gulp        = require('gulp');
-var exec        = require('child_process').exec;
-var dotenv      = require('dotenv').load({ silent: true });
+const $ = require('gulp-load-plugins')({ lazy: true });
+const os = require('os');
+const del = require('del');
+const gulp = require('gulp');
+const { exec } = require('child_process');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
-// var stripLine   = require('gulp-strip-line');
-var runSequence = require('run-sequence');
+require('dotenv').load({ silent: true });
+
+// const stripLine   = require('gulp-strip-line');
+const runSequence = require('run-sequence');
 
 /**
  * Banner
  */
 
-var pkg = require('./package.json');
+const pkg = require('./package.json');
 
-var banner = [
+const banner = [
   '/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
   ' */',
-  ''
+  '',
 ].join('\n');
 
-var jqueryCheck = [
+const jqueryCheck = [
   'if (typeof jQuery === \'undefined\') {',
   '  throw new Error(\'Bootstrap\\\'s JavaScript requires jQuery\');',
   '}',
-  ''
+  '',
 ].join('\n');
 
-var jqueryVersionCheck = [
+const jqueryVersionCheck = [
   '+function ($) {',
   '  var version = $.fn.jquery.split(\' \')[0].split(\'.\');',
   '  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1)) {',
@@ -48,16 +50,16 @@ var jqueryVersionCheck = [
   '}(jQuery);',
   '',
   '+function ($) {',
-  ''
+  '',
 ].join('\n');
 
-var footer = '\n\n}(jQuery);';
+const footer = '\n\n}(jQuery);';
 
 /**
  * Paths
  */
 
-var paths = {
+const paths = {
   clean: [
     'public/js/**/*.js',
     'public/js/**/*.map',
@@ -74,7 +76,7 @@ var paths = {
     'public/css/**/*.min.map',
   ],
   es6: [
-    'public/lib/bootstrap/js/src/*.js'
+    'public/lib/bootstrap/js/src/*.js',
   ],
   js: [
 
@@ -90,38 +92,38 @@ var paths = {
     'node_modules/bootstrap/js/dist/scrollspy.js',
     'node_modules/bootstrap/js/dist/tab.js',
     'node_modules/bootstrap/js/dist/tooltip.js',
-    'node_modules/bootstrap/js/dist/popover.js'
+    'node_modules/bootstrap/js/dist/popover.js',
 
     // 'public/lib/app/app.js'
   ],
   lint: [
-    'server/bin/**/*.js',
-    'test/**/*.js',
-    'server/routes/**/*.js',
-    'server/models/**/*.js',
-    'server/app.js',
-    'server/config.js',
-    'gulpfile.js'
-  ],
-  less: [
-    'less/main.less',
-    'less/landing.less',
-    'less/page-enrollment.less'
+    // 'server/bin/**/*.js',
+    // 'test/**/*.js',
+    // 'server/routes/**/*.js',
+    // 'server/models/**/*.js',
+    // 'server/app.js',
+    // 'server/config.js',
+    'gulpfile.js',
   ],
   scss: [
-    'server/scss/main.scss'
-  ]
+    'scss/main.scss',
+  ],
 };
 
 /**
  * Clean
  */
 
-gulp.task('clean', function () {
+gulp.task('clean', () => {
   return del(paths.clean);
 });
 
-gulp.task('copy', function () {
+
+/**
+ * Copy
+ */
+
+gulp.task('copy', () => {
   // popper
   gulp.src('./node_modules/popper.js/dist/umd/popper.min.*')
     .pipe(gulp.dest('./public/lib/popper.js'));
@@ -144,26 +146,24 @@ gulp.task('copy', function () {
  * Process CSS
  */
 
-gulp.task('styles', function () {
-  var autoprefixer = require('autoprefixer');
-  var cssnano      = require('cssnano');
-  var plugins      = [
+gulp.task('styles', () => {
+  const plugins = [
     autoprefixer({ browsers: ['last 2 versions'] }),
     cssnano({
-        preset: 'default',
-    })
+      preset: 'default',
+    }),
   ];
 
   return gulp.src(paths.scss)               // Read in scss files
     .pipe($.sass()
-    .on('error', $.sass.logError))          // Compile scss files
+      .on('error', $.sass.logError))        // Compile scss files
     // .pipe($.header(banner, { pkg: pkg }))   // Add banner
     .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
+    .pipe($.size({ title: 'CSS:' }))        // What size are we at?
     .pipe($.sourcemaps.init())
     .pipe($.postcss(plugins))               // Process CSS
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('./public/css'))        // Save CSS here
-    .pipe($.size({ title: 'CSS:' }))        // What size are we at?
     .pipe($.livereload());                  // Initiate a reload
 });
 
@@ -171,7 +171,7 @@ gulp.task('styles', function () {
  * transpile es6
  */
 
-gulp.task('transpile', function () {
+gulp.task('transpile', () => {
   return gulp.src(paths.es6)                // Get es6 src files
     // .pipe(stripLine([/^(import|export)/g]))
     .pipe($.babel({
@@ -180,13 +180,13 @@ gulp.task('transpile', function () {
           'env',
           {
             loose: true,
-            modules: false
-          }
-        ]
+            modules: false,
+          },
+        ],
       ],
       plugins: [
-        'transform-es2015-modules-strip'
-      ]
+        'transform-es2015-modules-strip',
+      ],
     }))                                     // transpile to es5
     .pipe(gulp.dest('./public/lib/bootstrap/js/dist'));
 });
@@ -195,10 +195,10 @@ gulp.task('transpile', function () {
  * Process Scripts
  */
 
-gulp.task('scripts', ['transpile'], function () {
+gulp.task('scripts', ['transpile'], () => {
   function createErrorHandler(name) {
-    return function (err) {
-      console.error('Error from ' + name + ' in scripts task', err.toString());
+    return (err) => {
+      console.error(`Error from, ${name}, in scripts task, ${err.toString()}`);
     };
   }
 
@@ -211,43 +211,62 @@ gulp.task('scripts', ['transpile'], function () {
     .pipe($.rename({ suffix: '.min' }))     // Add .min suffix
     .pipe($.uglify())                       // Minify the .js
     .on('error', createErrorHandler('uglify'))
-    .pipe($.header(banner, { pkg: pkg }))   // Add banner
+    .pipe($.header(banner, { pkg }))        // Add banner
     .pipe($.size({ title: 'JS:' }))         // What size are we at?
     .pipe(gulp.dest('./public/js'))         // Save minified .js
     .pipe($.livereload());                  // Initiate a reload
 });
 
-/**
- * JSHint Files
- */
+// /**
+//  * JSHint Files
+//  */
+//
+// gulp.task('lint', function () {
+//   return gulp.src(paths.lint)               // Read .js files
+//     .pipe($.jshint())                       // lint .js files
+//     .pipe($.jshint.reporter('jshint-stylish'));
+// });
+//
+// /**
+//  * JSCS Files
+//  */
+//
+// gulp.task('jscs', function () {
+//   return gulp.src(paths.lint)               // Read .js files
+//     .pipe($.jscs({ fix: true }))
+//     .pipe($.jscs.reporter());
+// });
 
-gulp.task('lint', function () {
-  return gulp.src(paths.lint)               // Read .js files
-    .pipe($.jshint())                       // lint .js files
-    .pipe($.jshint.reporter('jshint-stylish'));
+
+gulp.task('lint', () => {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(paths.lint)
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe($.eslint('.eslintrc'))
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe($.eslint.format());
+  // To have the process exit with an error code (1) on
+  // lint error, return the stream and pipe to failAfterError last.
+  // .pipe(eslint.failAfterError());
 });
 
-/**
- * JSCS Files
- */
-
-gulp.task('jscs', function () {
-  return gulp.src(paths.lint)               // Read .js files
-    .pipe($.jscs({ fix: true }))
-    .pipe($.jscs.reporter());
-});
 
 /**
  * Test Task
  */
 
-gulp.task('mocha', function () {
+gulp.task('mocha', () => {
   return gulp.src('./test/*.js', { read: false })
     .pipe($.mocha())
-    .once('error', function () {
+    .once('error', () => {
       process.exit(1);
     })
-    .once('end', function () {
+    .once('end', () => {
       process.exit(0);
     });
 });
@@ -256,8 +275,8 @@ gulp.task('mocha', function () {
  * Instanbul Code Coverage
  */
 
-gulp.task('test', function (cb) {
-  exec('istanbul cover _mocha -- -R spec', function (err, stdout, stderr) {
+gulp.task('test', (cb) => {
+  exec('istanbul cover _mocha -- -R spec', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     cb(err);  // finished task
@@ -269,61 +288,72 @@ gulp.task('test', function (cb) {
  *   - Build all the things...
  */
 
-gulp.task('build', function (cb) {
+gulp.task('build', (cb) => {
   runSequence(
     'clean',                     // first clean
-    ['copy', 'lint', 'jscs'],            // then lint and jscs
+    ['copy', 'lint'],            // then lint and jscs
     ['styles', 'scripts'],       // Then styles and scripts
-    // ['test'],
-    cb);
+    // ['mocha'],
+    cb,
+  );
 });
 
 /**
  * Nodemon Task
  */
 
-gulp.task('nodemon', ['build'], function (cb) {
+gulp.task('nodemon', ['build'], (cb) => {
+  let called = false;
+  const debug = `${pkg.name}:*`;
+
   $.livereload.listen();
-  var called = false;
+
   $.nodemon({
     script: './server/bin/www',
     verbose: false,
-    env: { DEBUG: pkg.name + ':*' },
+    env: { DEBUG: debug },
     ext: 'js',
     ignore: [
       'gulpfile.js',
       'public/',
-      'views/',
-      'less/',
-      'node_modules/'
-    ]
+      'server/views/',
+      'scss/',
+      'node_modules/',
+    ],
   })
-  .on('start', function () {
-    setTimeout(function () {
-      if (!called) {
-        called = true;
-        cb();
-      }
-    }, 3000);  // wait for start
-  })
-  .on('restart', function () {
-    setTimeout(function () {
-      $.livereload.changed('/');
-    }, 3000);  // wait for restart
-  });
+    .on('start', () => {
+      setTimeout(() => {
+        if (!called) {
+          called = true;
+          cb();
+        }
+      }, 3000);  // wait for start
+    })
+    .on('restart', () => {
+      setTimeout(() => {
+        $.livereload.changed('/');
+      }, 3000);  // wait for restart
+    });
 });
+
 
 /**
  * Open the browser
  */
 
-var browser = os.platform() === 'linux' ? 'google-chrome' : (
-  os.platform() === 'darwin' ? 'google chrome' : (
-  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+gulp.task('open', ['nodemon'], () => {
+  let browser = '';
 
-gulp.task('open', ['nodemon'], function () {
+  if (os.platform() === 'linux') {
+    browser = 'google-chrome';
+  } else if (os.platform() === 'darwin') {
+    browser = 'google chrome';
+  } else if (os.platform() === 'win32') {
+    browser = 'chrome';
+  }
+
   gulp.src('')
-  .pipe($.open({ app: browser, uri: 'http://localhost:3000' }));
+    .pipe($.open({ app: browser, uri: 'http://localhost:3000' }));
 });
 
 
@@ -331,9 +361,9 @@ gulp.task('open', ['nodemon'], function () {
  * Default Task
  */
 
-gulp.task('default', ['open'], function () {
+gulp.task('default', ['open'], () => {
   gulp.watch('server/scss/**/*.scss', ['styles']);
   gulp.watch(paths.es6, ['scripts']);
-  gulp.watch(paths.lint, ['lint', 'jscs']);
-  gulp.watch('server/views/**/*.jade').on('change', $.livereload.changed);
+  gulp.watch(paths.lint, ['lint']);
+  gulp.watch('server/views/**/*.pug').on('change', $.livereload.changed);
 });
